dc6b25f65ad1846425db200a96de2b5915846d44
LosmanDev <losman78@gmail.com>
2022-07-01 16:13:23 -0400

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -39,11 +39,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
       }
     });
   });
-
-  // Activate SimpleLightbox plugin for portfolio items
-  new SimpleLightbox({
-    elements: "#portfolio a.portfolio-box",
-  });
 });
 
 //EmailJS
